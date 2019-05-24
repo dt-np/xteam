03436c2f8ff5fe8e9088283db4172fbcb30482a5
amitraahul <amitraahul@lxslc614.ihep.ac.cn>
2019-05-24 09:40:25 +0800

    update

--- submit.sh
+++ submit.sh
@@ -14,11 +14,10 @@ usage() {
     printf "\n\t%-9s  %-40s"  "0.1.1"    "simulation --10 signal MC sample interactively for jpsiantisigmaminussigmaplus events..."
     printf "\n\t%-9s  %-40s"  "0.1.2"    "Generate -- 20000 jpsiantisigmaminussigmaplus MC signal..."
     printf "\n\t%-9s  %-40s"  "0.1.3"    "Reconstruction -- 20000 jpsiantisigmaminussigmaplus MC signal..."
-    printf "\n\t%-9s  %-40s"  "0.1.4"    "Preselection for 20k events -- generate root file [Checking interactively]..."
+    printf "\n\t%-9s  %-40s"  "0.1.4"    "Preselection for 10 events -- generate root file [Checking interactively]..."
     printf "\n\t%-9s  %-40s"  "0.1.5"    "Preselection for 20k events -- generate root file [cluster job]..."
 
     printf "\n\n" 
-
 
 }
 
@@ -50,7 +49,7 @@ case $option in
             then
             echo "now in yes"
 
-            cd scripts/jpsiantisigmaminussigmaplus/jobs_jpsiantisigmaminussigmaplus
+            cd scripts/jpsiantisigmaminussigmaplus/jobs_scripts
             boss.exe jobOptions_sim_jpsiantisigmaminussigmaplus.txt
             cd $HOME/bes/hypermiss
         else
@@ -66,7 +65,7 @@ case $option in
             then
             echo "now in yes"
         
-            cd scripts/jpsiantisigmaminussigmaplus/jobs_jpsiantisigmaminussigmaplus
+            cd scripts/jpsiantisigmaminussigmaplus/jobs_scripts
             boss.condor -g physics jobOptions_sim_jpsiantisigmaminussigmaplus.txt
             cd $HOME/bes/hypermiss
         else
@@ -76,20 +75,31 @@ case $option in
     
     0.1.3) echo "reconstruction -- generate 20000 signal MC sample..."
             
-	    cd scripts/jpsiantisigmaminussigmaplus/jobs_jpsiantisigmaminussigmaplus
+	    cd scripts/jpsiantisigmaminussigmaplus/jobs_scripts
         boss.condor -g physics jobOptions_rec_jpsiantisigmaminussigmaplus.txt
         ;;
 
-    0.1.4) echo "Preselection for 20k events -- generate root file [Checking interactively]..."
+    0.1.4) echo "Preselection for 10 events -- generate root file [Checking interactively]..."
 	
-	    cd scripts/jpsiantisigmaminussigmaplus/jobs_jpsiantisigmaminussigmaplus/
+	    cd scripts/jpsiantisigmaminussigmaplus/jobs_scripts
 	    boss.exe jobOptions_jpsiantisigmaminussigmaplus_gen_mc.txt
         ;;
 
     0.1.5) echo "Preselection for 20k events -- generate root file [cluster job]..."
 	
-	    cd scripts/jpsiantisigmaminussigmaplus/jobs_jpsiantisigmaminussigmaplus/
+        echo "have you changed event number from 10 which was previously set for interactive job?(yes / NO)"
+        
+        read opt
+        if [ $opt == "yes" ]
+            then
+            echo "now in yes"
+
+	    cd scripts/jpsiantisigmaminussigmaplus/jobs_scripts
 	    boss.condor -g physics jobOptions_jpsiantisigmaminussigmaplus_gen_mc.txt
+        cd $HOME/bes/hypermiss
+        else
+            echo "Default value is 'NO', please change the event number"
+        fi
         ;;
 
 
