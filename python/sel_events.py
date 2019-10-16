27df2b48b884d7fad1282c7c4f9b60cd69e06e6c
amitraahul <amitraahul@lxslc607.ihep.ac.cn>
2019-10-16 19:53:39 +0800

    selection script to produce a tree with variables mgamgam,mxi0,mrecxi0 and mlambda

--- python/sel_events.py
+++ python/sel_events.py
@@ -19,19 +19,19 @@ MJPSI = 3.097 #GeV
 ECMS = 3.09 #GeV
 
 #global histogram
-h_run = ROOT.TH1D('h_run','run',100, 0, 25330)
-h_event = ROOT.TH1D('h_event', 'event', 100,0 ,10000000)
-h_ncharged = ROOT.TH1D('h_ncharged', 'ncharged', 100, 1, 10)
-h_ngoodcharged = ROOT.TH1D('h_ngoodcharged', 'ngcharged',100, 0, 10)
-h_nshower = ROOT.TH1D('h_nshower', 'nshw', 100, 1, 10)
-h_ngoodshower = ROOT.TH1D('h_ngoodshower', 'ngshw',100, 0, 10)
-h_ne = ROOT.TH1D('h_ne', 'ne', 100, -1, 3)
-h_energy_gamma1 = ROOT.TH1D('h_energy_gamma1', 'energy_gamma1', 100, 0, 2)
-h_energy_gamma2 = ROOT.TH1D('h_energy_gamma2', 'energy_gamma2', 100, 0, 2)
-h_pi0 = ROOT.TH1D ('h_pi0','mass_gam12',100, 0, 0.5)
-h_lambda = ROOT.TH1D ('h_lambda','mass_lambda',100, 1, 1.3)
-h_xi0 = ROOT.TH1D ('h_xi0','mass_xi0',2000, 1, 3)
-h_rec_mass_xi0 = ROOT.TH1D ('h_rec_mass_xi0','rec_mass_xi0',100, 0, 2)
+# h_run = ROOT.TH1D('h_run','run',100, 0, 25330)
+# h_event = ROOT.TH1D('h_event', 'event', 100,0 ,10000000)
+# h_ncharged = ROOT.TH1D('h_ncharged', 'ncharged', 100, 1, 10)
+# h_ngoodcharged = ROOT.TH1D('h_ngoodcharged', 'ngcharged',100, 0, 10)
+# h_nshower = ROOT.TH1D('h_nshower', 'nshw', 100, 1, 10)
+# h_ngoodshower = ROOT.TH1D('h_ngoodshower', 'ngshw',100, 0, 10)
+# h_ne = ROOT.TH1D('h_ne', 'ne', 100, -1, 3)
+# h_energy_gamma1 = ROOT.TH1D('h_energy_gamma1', 'energy_gamma1', 100, 0, 2)
+# h_energy_gamma2 = ROOT.TH1D('h_energy_gamma2', 'energy_gamma2', 100, 0, 2)
+# h_pi0 = ROOT.TH1D ('h_pi0','mass_gam12',100, 0, 0.5)
+# h_lambda = ROOT.TH1D ('h_lambda','mass_lambda',100, 1, 1.3)
+# h_xi0 = ROOT.TH1D ('h_xi0','mass_xi0',2000, 1, 3)
+# h_rec_mass_xi0 = ROOT.TH1D ('h_rec_mass_xi0','rec_mass_xi0',100, 0, 2)
 
 mgamgam = ROOT.vector('double')()
 p4gam = ROOT.vector('double')()
@@ -67,6 +67,11 @@ def main ():
     t_out.Branch('p4truth', p4truth)
     t_out.Branch('p4truthgam', p4truthgam)
 
+    n_run = array('i',[0])
+    n_event = array('i',[0])
+    t_out.Branch("run", n_run, "run/I")
+    t_out.Branch("event", n_event, "event/I")
+
     
     pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=entries).start()
     time_start = time()
@@ -78,19 +83,12 @@ def main ():
     for k in range(entries):
 #    for k in range(1):
         pbar.update(k+1)
-        chain.GetEntry(k)
-        h_run.Fill(chain.run)
-        h_event.Fill(chain.event)
-        h_ncharged.Fill(chain.ncharged)
-        h_ngoodcharged.Fill(chain.ngcharged)
-        h_nshower.Fill(chain.nshw)
-        h_ngoodshower.Fill(chain.ngshw)
-        h_ne.Fill(chain.ne)
+        chain.GetEntry(k)         
         
 	p4_lambda =ROOT.TLorentzVector(chain.p4lambda[0],chain.p4lambda[1],chain.p4lambda[2],chain.p4lambda[3])
         mass_lambda = p4_lambda.M()
-        h_lambda.Fill(mass_lambda)
-    
+        n_run[0] = chain.run
+        n_event[0] = chain.event
         # setting gammas with p4shower information
         #the most energetic photon selection by comparing the energy of photons
         tempindexgam1=0
@@ -140,26 +138,21 @@ def main ():
         p4shw_gam12 = p4shw_gam1 + p4shw_gam2
     
         mass_gam12 = p4shw_gam12.M()
- 	h_pi0.Fill(mass_gam12)
 
- 	energy_gamma1 = p4shw_gam1.E()
- 	energy_gamma2 = p4shw_gam2.E()
- 	h_energy_gamma1.Fill(energy_gamma1)
- 	h_energy_gamma2.Fill(energy_gamma2)
-
+        
  	p4_xi0 = p4shw_gam12 + p4_lambda
  	mass_xi0 = p4_xi0.M()
- 	h_xi0.Fill(mass_xi0)
-
+ 
  	p4_rec_xi0 = cms_p4 - p4_xi0
  	rec_mass_xi0 = p4_rec_xi0.M()
- 	h_rec_mass_xi0.Fill(rec_mass_xi0)
-
+ 
 	mgamgam.push_back(mass_gam12)
-	for i in range(4):
+   	
+        for i in range(4):
 	    p4gam.push_back(chain.p4shw[tempindexgam1+i])
 	for i in range(4):
 	    p4gam.push_back(chain.p4shw[tempindexgam2+i])
+     
 	mxi0.push_back(mass_xi0)
 	mrecxi0.push_back(rec_mass_xi0)
 	mlambda.push_back(mass_lambda)
@@ -193,40 +186,23 @@ def main ():
 ##		cut_pi=(mass_gam12 < 0.125 or mass_gam12 > 0.145)
 ##		if cut_pi:
 ##		    continue
-#        	h_pi0.Fill(mass_gam12)
-#
 #        	energy_gamma1 = p4shw_gam1.E()
 #        	energy_gamma2 = p4shw_gam2.E()
-#        	h_energy_gamma1.Fill(energy_gamma1)
-#        	h_energy_gamma2.Fill(energy_gamma2)
+
 #
 #        	p4_xi0 = p4shw_gam12 + p4_lambda
 #        	mass_xi0 = p4_xi0.M()
-#        	h_xi0.Fill(mass_xi0)
+
 #
 #        	p4_rec_xi0 = cms_p4 - p4_xi0
 #        	rec_mass_xi0 = p4_rec_xi0.M()
-#        	h_rec_mass_xi0.Fill(rec_mass_xi0)
+
 #
 #		t_out.Fill()
 #		mgamgam.clear()
 
     t_out.Write()
    
-    h_run.Write()
-    h_event.Write()
-    h_ncharged.Write()
-    h_ngoodcharged.Write()
-    h_nshower.Write()
-    h_ngoodshower.Write()
-    h_ne.Write()
-    h_energy_gamma1.Write()
-    h_energy_gamma2.Write()
-    h_pi0.Write()
-    h_lambda.Write()
-    h_xi0.Write()
-    h_rec_mass_xi0.Write()
-
     fout.Close()
     pbar.finish()
     dur = duration(time()-time_start)
