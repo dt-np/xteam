e114e5a8e63d5f5c5d1940005e23ffcab0cd651d
amitraahul <amitraahul@lxslc605.ihep.ac.cn>
2019-11-06 15:30:43 +0800

    pi0 candidate which have closest mass to pdg value defined in a different function mass_diff_pi0

--- python/sel_events.py
+++ python/sel_events.py
@@ -16,17 +16,60 @@ import sys
 import os
 import math
 
-
 MJPSI = 3.097  # GeV
 ECMS = 3.09  # GeV
 
-ngamgam = array('i', [0])
-mgamgam = array('d',100*[0])
+n_run = array('i', [0])
+n_event = array('i', [0])
+n_indexmc = array('i',[0])
+n_pdgid = array('i', 100*[-99])
+n_motheridx = array('i', 100*[-99])
+#ngamgam = array('i', [0])
+mgamgam = array('d',[0])
 t_out = ROOT.TTree('pi0', 'pi0')
-t_out.Branch('ngamgam', ngamgam,"ngamgam/I")
-t_out.Branch('mgamgam', mgamgam,"mgamgam[ngamgam]/D")
+t_out.Branch("run", n_run, "run/I")
+t_out.Branch("event", n_event, "event/I")
+t_out.Branch("indexmc", n_indexmc, "indexmc/I")
+t_out.Branch("pdgid", n_pdgid, "pdgid[100]/I")
+t_out.Branch("motheridx", n_motheridx, "motheridx[100]/I")
+#t_out.Branch('ngamgam', ngamgam,"ngamgam/I")
+t_out.Branch('mgamgam', mgamgam,"mgamgam/D")
 
 # loop through each gamma photons to reconstruct pi0 candidates
+
+def mass_diff_pi0(chain):
+    tempindexgshw1=-1
+    tempindexgshw2=-1
+    tempminmassdifference=999.
+    for l in range(chain.ngshw):
+        for m in range(chain.ngshw):
+            if l>=m:
+                continue
+            indexgshw1 = l*6
+            indexgshw2 = m*6
+
+            p4shw_gam1 = ROOT.TLorentzVector(chain.p4shw[indexgshw1],chain.p4shw[indexgshw1+1],chain.p4shw[indexgshw1+2],chain.p4shw[indexgshw1+3])
+            p4shw_gam2 = ROOT.TLorentzVector(chain.p4shw[indexgshw2],chain.p4shw[indexgshw2+1],chain.p4shw[indexgshw2+2],chain.p4shw[indexgshw2+3])
+            p4shw_gam12 = p4shw_gam1 + p4shw_gam2
+            
+            if tempminmassdifference>abs(p4shw_gam12.M()-0.1349766):
+                tempindexgshw1=l*6
+                tempindexgshw2=m*6
+                tempminmassdifference=abs(p4shw_gam12.M()-0.1349766)
+    if tempindexgshw2!=-1 and tempindexgshw1!=-1:
+        p4shw_gam1 = ROOT.TLorentzVector(chain.p4shw[tempindexgshw1],chain.p4shw[tempindexgshw1+1],chain.p4shw[tempindexgshw1+2],chain.p4shw[tempindexgshw1+3])
+        p4shw_gam2 = ROOT.TLorentzVector(chain.p4shw[tempindexgshw2],chain.p4shw[tempindexgshw2+1],chain.p4shw[tempindexgshw2+2],chain.p4shw[tempindexgshw2+3])
+        p4shw_gam12 = p4shw_gam1 + p4shw_gam2
+        mgamgam[0]=p4shw_gam12.M()
+#        print chain.run, chain.event
+#        print chain.p4shw[tempindexgshw1],chain.p4shw[tempindexgshw1+1],chain.p4shw[tempindexgshw1+2],chain.p4shw[tempindexgshw1+3]
+#        print chain.p4shw[tempindexgshw2],chain.p4shw[tempindexgshw2+1],chain.p4shw[tempindexgshw2+2],chain.p4shw[tempindexgshw2+3]
+#        print p4shw_gam12.M()
+    else:
+        mgamgam[0]=-666
+    t_out.Fill()
+
+
 def mass_loop_pi0(chain):
     #print('\n')
     for l in range(chain.ngshw):
@@ -63,12 +106,6 @@ def main():
     fout = ROOT.TFile(outfile, "RECREATE")
     entries = chain.GetEntries()
     
-    n_run = array('i', [0])
-    n_event = array('i', [0])
-    n_indexmc = array('i',[0])
-    t_out.Branch("run", n_run, "run/I")
-    t_out.Branch("event", n_event, "event/I")
-    t_out.Branch("indexmc", n_indexmc, "indexmc/I")
        
     pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=entries).start()
     time_start = time()
@@ -79,9 +116,13 @@ def main():
         n_run[0] = chain.run
         n_event[0] = chain.event
         n_indexmc[0] = chain.indexmc
+	for ii in range(n_indexmc[0]):
+	    n_pdgid[ii]=chain.p4truth[ii*6+4]
+	    n_motheridx[ii]=chain.p4truth[ii*6+5]
         
-        mass_loop_pi0(chain)
-        ngamgam[0]=0
+#	mass_loop_pi0(chain)
+#       ngamgam[0]=0
+	mass_diff_pi0(chain)
           
     t_out.Write()
     t_out.Print()
