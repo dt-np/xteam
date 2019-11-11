e7b4d3a5287ae0390cb5bb84f96cd48efc977b9b
amitraahul <amitraahul@lxslc614.ihep.ac.cn>
2019-11-11 20:49:14 +0800

    Canvas colour is white now c1->SetFillColor(10);

--- plots/plot_pi0.c
+++ plots/plot_pi0.c
@@ -1,13 +1,13 @@
-
 {
 int N;
 //SetStyle();
 TFile f("../dat/run/signalMC/event_signalMC/jpsi2xi0_event.root", "read");
 TCanvas *c1 = new TCanvas("c1", "M_{#gamma#gamma}", 800, 600);
 gStyle->SetOptStat(0);
+c1->SetFillColor(10);
+c1->SetFrameFillColor(10);
 TTree* tree = (TTree*) f.Get("pi0");
-Double_t  xmin = 0.0;
-Double_t xmax = 0.3;
+Double_t  xmin = 0.0; xmax = 0.3;
 Int_t xbins = 300;
 Double_t ytitle;
 TH1F *h = new TH1F("h", "", xbins, xmin, xmax);
@@ -17,21 +17,12 @@ int run,ngamgam;
 tree->SetBranchAddress("mgamgam", x);
 tree->SetBranchAddress("run", &run);
 N=tree->GetEntries();
-//for (int i=0;i<20;i++){
-//	tree->GetEntry(i);
-//	cout<<i<<endl;
-//	if (ngamgam>0){
-//		cout<<run<<" "<<" "<<x[0]<<endl;
-//	}
-//}
 tree->Draw("mgamgam>>h");
 h->SetLineColor(kRed);
 //h->Print();
 h->SetXTitle("M_{#gamma#gamma} (GeV/c^{2})");
 h->SetYTitle("Events/ (1 Mev/c^{2})");//1 Mev
 h->SetFillColor(10);
-//h->Rebin(2);
-//c1->cd();
 h->Draw("");
 c1->Update();
 c1->Print("gamgam.pdf");
