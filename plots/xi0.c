cafeddbc6527d29b252a1154aed1b3e8a83f9cb9
amitraahul <amitraahul@lxslc607.ihep.ac.cn>
2019-12-03 11:16:41 +0800

    Plots with some changes advised by kai for Xi0 and rec_xi0

--- plots/xi0.c
+++ plots/xi0.c
@@ -6,8 +6,8 @@ gStyle->SetOptStat(0);
 c1->SetFillColor(10);
 c1->SetFrameFillColor(10);
 TTree* tree = (TTree*) f.Get("pi0");
-Double_t  xmin = 1.2; 
-Double_t xmax = 1.5;
+Double_t  xmin = 1.20; 
+Double_t xmax = 1.45;
 Int_t xbins = 200;
 Double_t ytitle;
 TH1F *h = new TH1F("h", "", xbins, xmin, xmax);
@@ -18,9 +18,16 @@ tree->Draw("mxi0>>h"," mlambda>1.11 && mlambda<1.12 ");
 h->SetLineColor(kRed);
 h->Print();
 h->SetXTitle("M_{#Xi^{0}} (GeV/c^{2})");
-h->SetYTitle("Events/ (1.5 Mev/c^{2})");  //1.5 Mev
+h->SetYTitle("Events/ (1.25 Mev/c^{2})");  //1.25 Mev
 h->SetFillColor(10);
 h->Draw("");
+TArrow arrow1,arrow2;
+arrow1.SetLineColor(2);
+arrow1.SetLineWidth(3);
+arrow1.DrawArrow(1.25,50,1.25,0,0.02,"->");    //the begin point and end point's location,line side,line style...
+arrow2.SetLineColor(2);
+arrow2.SetLineWidth(3);
+arrow2.DrawArrow(1.40,50,1.40,0,0.02,"->");    //the begin point and end point's location,line side,line style...
 c1->Update();
 c1->Print("Xi0.pdf");
 }
